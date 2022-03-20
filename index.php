93b61d7e484cf40548a57e5912dc2263b27a09e1
Carlos Ramirez <carlosramirez@Carloss-MacBook-Pro.local>
2022-03-20 10:05:38 -0700

    Updated index page

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once("config/heroku_db.php");
 $errors= [];
 $name = "";
 $email = "";
+$issue = "";
+$comment= "";
 $result ="";
 
  if(isset($_POST["submit"])) {
@@ -19,9 +21,11 @@ $result ="";
         //Makes sure the input is strings and then sets the variables to the user input 
      $name = mysqli_real_escape_string($conn, $_POST["username"]);
      $email = mysqli_real_escape_string($conn, $_POST["email"]);
+     $issue =  $_POST["issue"];
+     $comment =  mysqli_real_escape_string($conn, $_POST["comment"]);
 
      //Creates the sql query
-     $sql = "INSERT INTO login(name, email) VALUES('$name', '$email')";
+     $sql = "INSERT INTO login(name, email, issue, comment) VALUES('$name', '$email', '$issue', '$comment')";
 
      //save to db
 
@@ -60,13 +64,16 @@ $result ="";
               <?php echo $errors["email"] ?? ""  ?>  
             </div>
 
-            <label>Issue: </label>
+            <label>Issue:</label>
             <select name="issue" id="issue">
               <option value="query">Query</option>
               <option value="feedback">Feedback</option>
               <option value="complaint">Complaint</option>
               <option value="other">Other</option>
             </select>
+
+            <label>Comment:</label>
+            <textarea id="comment" name="comment" cols="30" rows="10" value="<?php echo htmlspecialchars($_POST["comment"] ?? "") ?>"><?php echo htmlspecialchars($_POST["comment"] ?? "") ?></textarea>
 
             <input type="submit" value="submit" name="submit">
         </form>
