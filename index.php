c21e4924dd24875ea0b51e3d8e176e084a4b60e3
Carlos Ramirez <carlosramirez@Carloss-MacBook-Pro.local>
2022-03-21 20:00:41 -0700

    Added id to the url string

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $result ="";
        //If no error success message is diplayed
        $new_id = mysqli_insert_id($conn); 
        $result =  "<div class='success'>Account Added</div>";
-      header("Location: details.php?=$new_id");
+      header("Location: details.php?id=$new_id");
        
      } else {
        //If there is an error is is diplayed
