8b2ada730ee70e7fdb75f8be34af063c4d420b9f
Carlos Ramirez <carlosramirez@Carloss-MacBook-Pro.local>
2022-03-26 09:01:47 -0700

    Testing sql query

--- details.php
+++ details.php
@@ -1,3 +1,20 @@
+<?php 
+
+require_once("config/heroku_db");
+
+//Get ID from URL
+$id = $_GET['id'];
+
+//Create MySQL query
+$sql = "SELECT * FROM login WHERE id = $id";
+
+//Send query
+$result = mysqli_query($conn, $sql);
+
+echo $result;
+
+?>
+
 <?php require_once("templates/header.php"); ?>
 <div class="new-user">
     <h2>Message Sent</h2>
