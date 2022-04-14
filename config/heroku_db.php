6477c128b1cc36e54e4ff48632f2bbefc59cf08e
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-04-13 20:23:08 -0700

    Added PDO creds

--- config/heroku_db.php
+++ config/heroku_db.php
@@ -7,4 +7,11 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-?>
+
+//DPO Setup
+
+//Create DSN
+$dsn = "mysql:host=$server;dbname=$db";
+
+//Create PDO Instance
+$pdo = new PDO($dsn, $user, $password);
